a02fc7f0d2dbb37d923baf3e6b056670b85cf4b9
carolhuang <carolhuang724@gmail.com>
2022-10-01 23:45:30 +0100

    del

--- 3-calculator-react/src/DigitButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import {ACTIONS} from './App'   // ACTIONS非默认暴露，一定要用花括号引起来
-
-// Clicking digits
-export default function DigitButton({dispatch, digit}) {
-  return (
-    <button onClick={()=>{dispatch({type:ACTIONS.ADD_DIGIT, payload:{digit}})}}>{digit}</button>
-  )
-}
